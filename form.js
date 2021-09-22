3470d87e3217d11f92a6729ad25c08d6349591f0
CarlosB <bhca0629@yahoo.es>
2021-09-21 22:50:45 -0500

    Adding form.js file

---
+++ form.js
@@ -0,0 +1,34 @@
+//Validación nombre de usuario
+
+function checkUsername(valor){
+
+}
+module.exports = checkUsername;
+
+
+//Validación contraseña
+
+function checkContrasena(valor){
+    
+}
+module.exports = checkContrasena;
+
+
+//Validación pregunta de seguridad
+
+
+
+
+//validación campo respuesta de seguridad
+
+function checkRespuestaSeg(valor){
+    
+}
+
+module.exports = checkRespuestaSeg;
+
+
+//validación integral de todas las funcionalidades
+
+
+
