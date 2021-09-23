8f09d859afdae7f72f3062cece2bc6642bfaf6f7
CarlosB <bhca0629@yahoo.es>
2021-09-22 21:34:26 -0500

    first update checkRespuestaSeg

--- form.js
+++ form.js
@@ -1,34 +1,57 @@
-//Validación nombre de usuario
+//VALIDACIÓN NOMBRE DE USUARIO
 
 function checkUsername(valor){
 
 }
-module.exports = checkUsername;
+//module.exports = checkUsername;
 
 
-//Validación contraseña
+
+//VALIDACIÓN CONTRASEÑA
 
 function checkContrasena(valor){
     
 }
-module.exports = checkContrasena;
+//module.exports = checkContrasena;
 
 
-//Validación pregunta de seguridad
+//VALIDACIÓN PREGUNTA SEGURIDAD
 
 
 
 
-//validación campo respuesta de seguridad
+//VALIDACIÓN CAMPO RESPUESTA DE SEGURIDAD
+
+//document.getElementById("btn-registrar").onclick=checkRespuestaSeg();
+var campoRespuesta = document.getElementById("respuesta-seguridad");
+
 
 function checkRespuestaSeg(valor){
-    
+
+    if(campoRespuesta.value != ""){
+            
+        let regxp = /^([a-zA-Z0-9_-]){1,30}$/;
+        if (regxp.test(campoRespuesta.value)==true){
+            document.getElementById("btn-registrar").focus();
+            alert("Campo respuesta seguridad: Correcto")
+            return true;
+        }else{
+            alert("El Campo respuesta debe contener máximo 30 caracteres y usar sólo letras o números");
+            document.getElementById("respuesta-seguridad").focus();
+            return false;
+        }
+    }else{
+        alert("el campo respuesta de seguridad no puede estar vacío")
+        document.getElementById("respuesta-seguridad").focus();
+        return false;
+      
+    }
 }
+    
+//module.exports = checkRespuestaSeg;
 
-module.exports = checkRespuestaSeg;
 
-
-//validación integral de todas las funcionalidades
+//VALIDACIÓN INTEGRAL DE TODAS LAS FUNCIONALIDADES
 
 
 
