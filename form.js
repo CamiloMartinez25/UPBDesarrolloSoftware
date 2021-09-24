b41cf9f92fdb39d161cedd6c9c094ab4b69e11d0
Camilo Andres Martinez <camilo@nebulaent.local>
2021-09-23 22:30:03 -0500

    adding validations to the form

--- form.js
+++ form.js
@@ -1,87 +1,85 @@
 //VALIDACIÓN NOMBRE DE USUARIO
 
-function checkUsername(valor){
-    let campoUsername = document.getElementById("username");
-     let regxp = /^([a-zA-Z0-9]){1,30}$/;
+function checkUsername(valor) {
+  //let campoUsername = document.getElementById("username"); no se debe llamar al campo porque ya estas recibiendo el valor
+  let regxp = /^([a-zA-Z0-9]){1,30}$/;
+  const usernameError = document.querySelector("#usernameError");
 
-    if(campoUsername.value != ""){
+  //if(campoUsername.value != ""){ Debe validarse el valor que se recibe y no el obtenido del campo directamente
 
-      
-       if (regxp.test(campoUsername.value)==true){
-           document.getElementById("contrasena").focus();
-           alert("Campo username: Correcto")
-           return true;
-       }else{
-           alert("El Campo username debe contener máximo 30 caracteres y usar sólo letras o números");
-           document.getElementById("username").focus();
-           return false;
-       }
-   }else{
-       alert("El campo username no puede estar vacío")
-       document.getElementById("username").focus();
-       return false;
+  if (valor) {
+    if (regxp.test(valor) == true) {
+      usernameError.textContent = "";
+      return true;
+    } else {
+      usernameError.textContent =
+        "El Campo username debe contener máximo 30 caracteres y usar sólo letras o números";
+      document.getElementById("username").focus();
+      return false;
+    }
+  } else {
+    usernameError.textContent = "El campo username no puede estar vacío.";
+    document.getElementById("username").focus();
+    return false;
+  }
 
-   }
+  //Aqui dejo el codigo refactorizado para que lo analices
+  /* if (!valor) {
+    usernameError.textContent = "El campo username no puede estar vacío.";
+    document.getElementById("username").focus();
+    return false;
+  }
 
+  usernameError.textContent = regxp.test(valor) ? "" : "El Campo username debe contener máximo 30 caracteres y usar sólo letras o números";
+  return regxp.test(valor); */
 }
-//module.exports = checkUsername;
-
-
 
 //VALIDACIÓN CONTRASEÑA
 
-function checkContrasena(valor){
-    var passregex= /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})$/
-    if (valor.match(passregex)) {
-        alert("Válida");
-        return true;
-    }
-    else {
-        alert("No es válida");
-        return false;
-    }
+function checkContrasena(valor) {
+  const passregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})$/;
+  const contrasenaError = document.querySelector("#contrasenaError");
+
+  if (!valor) {
+    contrasenaError.textContent = "El campo contraseña no puede estar vacío.";
+    document.getElementById("contrasena").focus();
+    return false;
+  }
+
+  contrasenaError.textContent = valor.match(passregex) ? "" : "La contraseña debe contener almenos una letra mayúscula, una minúscula, un número y con una longitud mayor o igual a 8 dígitos"
+
+  if (valor.match(passregex)) {
+    return true;
+  } else {
+    return false;
+  }
+
+  //refactorización del codigo
+  //return valor.match(passregex);
 }
-
-//module.exports = checkContrasena;
-
 
 //VALIDACIÓN PREGUNTA SEGURIDAD
 
-
-
-
 //VALIDACIÓN CAMPO RESPUESTA DE SEGURIDAD
 
-//document.getElementById("btn-registrar").onclick=checkRespuestaSeg();
 
-function checkRespuestaSeg(valor){
-    
-    let campoRespuesta = document.getElementById("respuesta-seguridad");
-    let regxp = /^([a-zA-Z0-9]){1,30}$/;
-    
-    if(campoRespuesta.value != ""){
-                   
-        if (regxp.test(campoRespuesta.value)==true){
-            document.getElementById("btn-registrar").focus();
-            alert("Campo respuesta seguridad: Correcto");
-            return true;
-        }else{
-            alert("El Campo respuesta debe contener máximo 30 caracteres y usar sólo letras o números");
-            document.getElementById("respuesta-seguridad").focus();
-            return false;
-        }
-    }else{
-        alert("el campo respuesta de seguridad no puede estar vacío");
-        document.getElementById("respuesta-seguridad").focus();
-        return false;
-      
-    }
+function checkRespuestaSeg(valor) {
+  //let campoRespuesta = document.getElementById("respuesta-seguridad");  No se debe llamar el campo solo recibir el parametro a evaluar
+  let regxp = /^([a-zA-Z0-9]){1,30}$/;
+  const respuestaSeguridadError = document.querySelector("#respuestaSeguridadError");
+
+  if (!valor) {
+    respuestaSeguridadError.textContent = "El campo respuesta de seguridad no puede estar vacío.";
+    document.getElementById("respuesta-seguridad").focus();
+    return false;
+  }
+
+  respuestaSeguridadError.textContent = regxp.test(valor) ? '' : "El Campo respuesta debe contener máximo 30 caracteres y usar sólo letras o números";
+  return regxp.test(valor);
 }
-    
-//module.exports = checkRespuestaSeg;
 
-
-//VALIDACIÓN INTEGRAL DE TODAS LAS FUNCIONALIDADES
-
-
-
+module.exports = {
+  checkRespuestaSeg,
+  checkContrasena,
+  checkUsername,
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+const userNameInput = document.getElementById('username');
+const contrasenaInput = document.getElementById('contrasena');
+const preguntaSeguridadInput = document.getElementById('preguntas-seguridad');
+const respuestaSeguridadInput = document.getElementById('respuesta-seguridad');
+
+
+userNameInput.addEventListener('input', () => {
+    checkUsername(userNameInput.value);
+})
+
+contrasenaInput.addEventListener('input', () => {
+    checkContrasena(contrasenaInput.value);
+})
+
+preguntaSeguridadInput.addEventListener('input', () => {
+    checkpreguntaSeguridad(preguntaSeguridadInput.value);
+})
+
+respuestaSeguridadInput.addEventListener('input', () => {
+    checkRespuestaSeg(respuestaSeguridadInput.value);
+})
