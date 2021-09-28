7fc4d306e14eca994e7a8acb07257112b5db9a8f
Alejandro E. Wills F <ae.wills22@uniandes.edu.co>
2021-09-27 21:23:23 -0500

    Conjunto de datos de prueba

---
+++ testAW.js
@@ -0,0 +1,32 @@
+var arreglo = [
+  {
+    Username: "",
+    Contraseña: "Luke Skywalker",
+    PreguntaSeg: 98,
+    RespuestaSeg: 56,
+  },
+  {
+    Username: 82,
+    Contraseña: "Sabine Wren",
+    PreguntaSeg: 73,
+    RespuestaSeg: 99,
+  },
+  {
+    Username: 22,
+    Contraseña: "Zeb Orellios",
+    PreguntaSeg: 20,
+    RespuestaSeg: 59,
+  },
+  {
+    Username: 15,
+    Contraseña: "Ezra BrUsernameger",
+    PreguntaSeg: 43,
+    RespuestaSeg: 67,
+  },
+  {
+    Username: 11,
+    Contraseña: "Caleb Dume",
+    PreguntaSeg: 71,
+    RespuestaSeg: 85,
+  },
+];
