d44df462996d1c003f6b2cb008345abca91736fd
CarlosB <bhca0629@yahoo.es>
2021-09-27 23:11:01 -0500

    function agregarRegistro added

--- registro.js
+++ registro.js
@@ -1,8 +1,41 @@
 //ARREGLO DE OBJETOS
 
+var arreglo = new Array();
+
 function agregarRegistro() {
     
+    
+    let registroUsuario = new Array(4);
+    
+    registroUsuario[0] = document.getElementById('username').value;
+    registroUsuario[1] = document.getElementById('contrasena').value;
+    registroUsuario[2] = document.getElementById('preguntas-seguridad').value;
+    registroUsuario[3] = document.getElementById('respuesta-seguridad').value;
+
+       
+    arreglo.push(registroUsuario);
+    console.log(arreglo);
+
+    //esta sería la solución llenando el array principal con un diccionario en lugar de otro array
+
+    /*let username = document.getElementById('username').value;
+    let contrasena = document.getElementById('contrasena').value;
+    let preguntaSeguridad = document.getElementById('preguntas-seguridad').value;
+    let respuestaSeguridad = document.getElementById('respuesta-seguridad').value;
+
+    let registroUsuario = [
+
+        {name:'username', value:username},
+        {name:'password', value:contrasena},
+        {name:'preguntaSeguridad', value:preguntaSeguridad},
+        {name:'respuestaSeguridad', value:respuestaSeguridad}
+    ];
+    
+    arreglo.push(registroUsuario);
+    console.log(arreglo);*/
+    
 }
+
 
 //ORDENACIÓN DEL ARREGLO
 
