39e6595f7f227744dd7303a24259f44e5cac8115
Alejandro E. Wills F <ae.wills22@uniandes.edu.co>
2021-09-27 22:16:13 -0500

    Se arregla la mayúscula de Username a username para mantener el id.

--- testAW.js
+++ testAW.js
@@ -1,38 +1,38 @@
 var arreglo = [
   {
-    Username: "Skywalker",
-    Contrasena: "MmNnOo09",
+    username: "Skywalker",
+    contrasena: "MmNnOo09",
     PreguntaSeg: "¿Cuál es el apellido de soltera de tu mamá?",
     RespuestaSeg: "Soltera",
   },
   {
-    Username: "Sabine Wren",
-    Contrasena: "LlKkJj87",
+    username: "Sabine Wren",
+    contrasena: "LlKkJj87",
     PreguntaSeg: "¿En qué ano se casaron tus padres?",
     RespuestaSeg: "1980",
   },
   {
-    Username: "Orellios",
-    Contrasena: "PpOoIi65",
+    username: "Orellios",
+    contrasena: "PpOoIi65",
     PreguntaSeg: "¿Cuál fue tu primer número de teléfono?",
     RespuestaSeg: "2145507",
   },
   {
-    Username: "Ezra Bridger",
-    Contrasena: "BbVvCc43",
+    username: "Ezra Bridger",
+    contrasena: "BbVvCc43",
     PreguntaSeg: "¿Cuál era el nombre de tu primera profesora?",
     RespuestaSeg: "Victoria",
   },
   {
-    Username: "Caleb Dume",
-    Contrasena: "HhGgFf21",
+    username: "Caleb Dume",
+    contrasena: "HhGgFf21",
     PreguntaSeg: "¿Cuál era el modelo de tu primer carro?",
     RespuestaSeg: "Zastava",
   },
 ];
 
 function filtrarUsername(arreglo) {
-  return arreglo.filter(function (registro) {return registro.Username.length > 10 })
+  return arreglo.filter(function (registro) {return registro.username.length > 10 })
     
 }
 console.log(arreglo);
