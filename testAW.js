4f2565cfeb65cdde3310a5c53db356865f85d2af
Alejandro E. Wills F <ae.wills22@uniandes.edu.co>
2021-09-27 21:53:52 -0500

    Función filtrarUsername. Se usa .filter y la estructura de datos queda en testAW.js

--- testAW.js
+++ testAW.js
@@ -1,34 +1,42 @@
 var arreglo = [
   {
     Username: "Skywalker",
-    Contraseña: "MmNnOo09",
+    Contrasena: "MmNnOo09",
     PreguntaSeg: "¿Cuál es el apellido de soltera de tu mamá?",
     RespuestaSeg: "Soltera",
   },
   {
     Username: "Sabine Wren",
-    Contraseña: "LlKkJj87",
-    PreguntaSeg: "¿En qué año se casaron tus padres?",
+    Contrasena: "LlKkJj87",
+    PreguntaSeg: "¿En qué ano se casaron tus padres?",
     RespuestaSeg: "1980",
   },
   {
     Username: "Orellios",
-    Contraseña: "PpOoIi65",
+    Contrasena: "PpOoIi65",
     PreguntaSeg: "¿Cuál fue tu primer número de teléfono?",
     RespuestaSeg: "2145507",
   },
   {
     Username: "Ezra Bridger",
-    Contraseña: "BbVvCc43",
+    Contrasena: "BbVvCc43",
     PreguntaSeg: "¿Cuál era el nombre de tu primera profesora?",
     RespuestaSeg: "Victoria",
   },
   {
     Username: "Caleb Dume",
-    Contraseña: "HhGgFf21",
+    Contrasena: "HhGgFf21",
     PreguntaSeg: "¿Cuál era el modelo de tu primer carro?",
     RespuestaSeg: "Zastava",
   },
 ];
+
+function filtrarUsername(arreglo) {
+  return arreglo.filter(function (registro) {return registro.Username.length > 10 })
+    
+}
+console.log(arreglo);
+
+console.log(filtrarUsername(arreglo));
 
 
