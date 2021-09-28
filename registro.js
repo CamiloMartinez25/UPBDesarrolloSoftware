5e2727641975516c03472c4a06e8fc2fee47188d
CarlosB <bhca0629@yahoo.es>
2021-09-27 20:05:20 -0500

    file registros.js created

---
+++ registro.js
@@ -0,0 +1,35 @@
+//ARREGLO DE OBJETOS
+
+function agregarRegistro() {
+    
+}
+
+//ORDENACIÓN DEL ARREGLO
+
+function ordenarArreglo(arreglo) {
+    
+}
+
+
+//OBTENER REGISTROS CON NÚMEROS
+
+function obtenerUsername(arreglo) {
+    
+}
+
+
+//FILTRADO DE REGISTROS
+
+function filtrarUsername(arreglo) {
+    
+}
+
+
+//EXPORTACIÓN DE FUNCIONES
+
+module.exports = {
+    agregarRegistro,
+    ordenarArreglo,
+    obtenerUsername,
+    filtrarUsername
+  };
