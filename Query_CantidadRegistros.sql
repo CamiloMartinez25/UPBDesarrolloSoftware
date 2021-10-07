6dc228d3400d8bc206f678cb8394a467f0dfc332
Andres Echavarria <mac@iMac-de-Mac.local>
2021-10-06 21:05:22 -0500

    Queries actualizados

--- Query_CantidadRegistros.sql
+++ Query_CantidadRegistros.sql
@@ -1,2 +1,1 @@
-select count(*)
-  from users
+SELECT COUNT(*) FROM Usuarios;
--- Query_ObtenerUsername.sql
+++ Query_ObtenerUsername.sql
@@ -1,3 +1,1 @@
-SELECT username, id_security_question 
-FROM users
-order by username
+SELECT username, preg_Seguridad FROM Usuarios WHERE username REGEXP '[A-Za-z]+' AND username REGEXP '[0-9]+' ORDER BY username ASC;
